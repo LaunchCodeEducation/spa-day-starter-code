ad545aac596c017f9af8304444c548bc9198f923
Carly Langlois <cj.langlois.cl@gmail.com>
2019-12-04 17:01:26 -0600

    begin solution from older instructions

---
+++ src/main/java/org/launchcode/spaday/controllers/UserController.java
@@ -0,0 +1,21 @@
+package org.launchcode.spaday.controllers;
+
+import org.launchcode.spaday.models.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("user")
+public class UserController {
+
+    @GetMapping
+    public String displayAddUserForm(Model model) {
+        return "user/add";
+    }
+
+
+}
