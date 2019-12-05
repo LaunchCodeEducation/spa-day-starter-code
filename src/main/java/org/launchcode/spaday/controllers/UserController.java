5609353c75442cf811da95be14dc34d8d449174b
Carly Langlois <cj.langlois.cl@gmail.com>
2019-12-05 11:29:05 -0600

    add error message to user add page

--- src/main/java/org/launchcode/spaday/controllers/UserController.java
+++ src/main/java/org/launchcode/spaday/controllers/UserController.java
@@ -3,18 +3,30 @@ package org.launchcode.spaday.controllers;
 import org.launchcode.spaday.models.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("user")
 public class UserController {
 
-    @GetMapping
-    public String displayAddUserForm(Model model) {
+    @GetMapping("/add")
+    public String getAddUserForm(Model model) {
         return "user/add";
+    }
+
+    @PostMapping("/add")
+    public String postAddUserForm(Model model, @ModelAttribute User user, String verify) {
+        model.addAttribute("user", user);
+        model.addAttribute("verify", verify);
+        model.addAttribute("username", user.getUsername());
+        model.addAttribute("email", user.getEmail());
+        if (user.getPassword().equals(verify)) {
+            return "user/index";
+        } else {
+            model.addAttribute("error", "Passwords do not match");
+            return "user/add";
+        }
+
     }
 
 
