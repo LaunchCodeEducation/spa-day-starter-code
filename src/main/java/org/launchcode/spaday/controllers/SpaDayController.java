fc89a4e1e864e27e2b48cb8adbcad0b13d59cbc7
Carly Langlois <cj.langlois.cl@gmail.com>
2019-12-05 12:21:24 -0600

    remove name from starter code

--- src/main/java/org/launchcode/spaday/controllers/SpaDayController.java
+++ src/main/java/org/launchcode/spaday/controllers/SpaDayController.java
@@ -17,9 +17,9 @@ public class SpaDayController {
     }
 
     @PostMapping
-    public String spaMenu(@RequestParam String name, @RequestParam String skintype, @RequestParam String manipedi, Model model) {
+    public String spaMenu(@RequestParam String skintype, @RequestParam String manipedi, Model model) {
 
-        Client newClient = new Client(skintype, manipedi, name);
+        Client newClient = new Client(skintype, manipedi);
         newClient.setAppropriateFacials(skintype);
         model.addAttribute("client" , newClient);
 
