879da3813a067884c508fa00169ee05f240cdaf4
Geoffrey Meier <grm0809@gmail.com>
2023-02-17 16:30:22 -0600

    Studio update

--- src/main/java/org/launchcode/spaday/controllers/SpaDayController.java
+++ src/main/java/org/launchcode/spaday/controllers/SpaDayController.java
@@ -62,6 +62,10 @@ public class SpaDayController {
                 appropriateFacials.add(facials.get(i));
             }
         }
+        model.addAttribute("name", name);
+        model.addAttribute("skintype", skintype);
+        model.addAttribute("manipedi",manipedi);
+        model.addAttribute("appropriateFacials",appropriateFacials);
 
         return "menu";
     }
