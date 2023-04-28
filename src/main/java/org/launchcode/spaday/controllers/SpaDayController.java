658599f8dcd14388148027744760d4b6c4b4289a
Dan Hart <dan-hart@users.noreply.github.com>
2023-04-27 19:10:04 -0500

    Update SpaDayController.java

--- src/main/java/org/launchcode/spaday/controllers/SpaDayController.java
+++ src/main/java/org/launchcode/spaday/controllers/SpaDayController.java
@@ -1,5 +1,6 @@
 package org.launchcode.spaday.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -11,13 +12,31 @@ public class SpaDayController {
 
     public boolean checkSkinType(String skinType, String facialType) {
         if (skinType.equals("oily")) {
-            return facialType.equals("Microdermabrasion") || facialType.equals("Rejuvenating");
+            if (facialType.equals("Microdermabrasion") || facialType.equals("Rejuvenating")) {
+                return true;
+            }
+            else {
+                return false;
+            }
         }
         else if (skinType.equals("combination")) {
-            return facialType.equals("Microdermabrasion") || facialType.equals("Rejuvenating") || facialType.equals("Enzyme Peel");
+            if (facialType.equals("Microdermabrasion") || facialType.equals("Rejuvenating") || facialType.equals("Enzyme Peel")) {
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+        else if (skinType.equals("normal")) {
+            return true;
         }
         else if (skinType.equals("dry")) {
-            return facialType.equals("Rejuvenating") || facialType.equals("Hydrofacial");
+            if (facialType.equals("Rejuvenating") || facialType.equals("Hydrofacial")) {
+                return true;
+            }
+            else {
+                return false;
+            }
         }
         else {
             return true;
@@ -49,20 +68,26 @@ public class SpaDayController {
 
     @PostMapping(value="")
     public String spaMenu(@RequestParam String name, @RequestParam String skintype, @RequestParam String manipedi, Model model) {
+        String cap = skintype.substring(0, 1).toUpperCase() + skintype.substring(1);
 
-        ArrayList<String> facials = new ArrayList<>();
+        ArrayList<String> facials = new ArrayList<String>();
         facials.add("Microdermabrasion");
         facials.add("Hydrofacial");
         facials.add("Rejuvenating");
         facials.add("Enzyme Peel");
 
-        ArrayList<String> appropriateFacials = new ArrayList<>();
+        ArrayList<String> appropriateFacials = new ArrayList<String>();
         for (int i = 0; i < facials.size(); i ++) {
             if (checkSkinType(skintype,facials.get(i))) {
                 appropriateFacials.add(facials.get(i));
             }
         }
 
+        model.addAttribute("name" , name);
+        model.addAttribute("skintype", cap);
+        model.addAttribute("facials", facials);
+        model.addAttribute("appropriateFacials", appropriateFacials);
+        model.addAttribute("manipedi", manipedi);
         return "menu";
     }
 }
